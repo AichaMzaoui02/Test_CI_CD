ff02dcf4589b4a4a395595f1a921cc4f5d0c5977
MZAOUI <aicha.mzaoui@capgemini.com>
2026-01-05 12:22:40 +0100

    first commit

---
+++ src/test/java/com/example/Test_Ci_Cd/TestCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_Ci_Cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
